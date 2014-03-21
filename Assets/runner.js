4aa4d0a620591b6e28f0c934020df9bb6d8bd5f1
Ellis <el1t@ymail.com>
2014-03-21 19:04:41 -0400

    Highscores and new UI layout

--- Assets/runner.js
+++ Assets/runner.js
@@ -1,6 +1,8 @@
 ﻿#pragma strict
+import System.IO;
+
 @System.NonSerialized
-var board: Array;
+var board: int[];
 @System.NonSerialized
 var lost:boolean = false;
 @System.NonSerialized
@@ -13,81 +15,93 @@ var timer:double;
 var countDown:double;
 @System.NonSerialized
 var paused:boolean;
+@System.NonSerialized
+var scores:float[];
 
 var maxTime:int;
 var maxScore:int;
 var skin:GUISkin;
-var whiteSkin:GUISkin;
-var rock:Texture;
+var scoreBoard:TextAsset;
+var rock_textures:Texture[];
 
 function Start () {
-	var rows:int;
-	var cols:int;
+	var row:int;
+	var col:int;
 	var index:int;
 	paused = false;
 	score = 0;
 	time = 0;
 	timer = 2;
 	countDown = 2.5;
-	board = new Array(4);
-	for(rows = 0; rows < 4; rows++) {
+	board = new int[16];
+	for(row = 0; row < 4; row++) {
 		index = Random.Range(0,4);
-		board[rows] = new Array();
-		for(cols = 0; cols < 4; cols++) {
-			if(index == cols) {
-				(board[rows] as Array)[cols] = 1;
+		for(col = 0; col < 4; col++) {
+			if(index == col) {
+				board[row*4 + col] = Random.Range(1, rock_textures.Length);
 			} else {
-				(board[rows] as Array)[cols] = 0;
+				board[row*4 + col] = 0;
 			}
 		}
 	}
+	
+	//build highscores
+	scores = new float[10];
+	var temp = scoreBoard.text.Split("\n"[0]);
+	for(var i=0; i < temp.Length; i++) {
+		if(temp[i] == "")
+			break;
+		scores[i] = parseFloat(temp[i]);
+	}
+
+	// integrity check
 }
 
 function OnGUI () {
+	GUI.skin = skin;
+
+	// game over
 	if(lost) {
-		GUI.skin = whiteSkin;
 		GUI.backgroundColor.a = 0;
 		if (timer <= 0 && Event.current.type == EventType.KeyDown)
-			lose();
+			exit();
 		var endResult = "You lost!";
-		if(maxTime > 0 && time > maxTime || maxScore > 0 && score >= maxScore)
+		if(maxTime > 0 && time >= maxTime || maxScore > 0 && score >= maxScore)
 			endResult = "Finish!\nScore: " + score + "\nTime: " +
 				Mathf.Round(time*1000)/1000 + " seconds.\nTiles per second: " + Mathf.Round(score/time*100)/100;
-		if(GUI.Button(Rect(0,0,Screen.width,Screen.height), endResult ))
-			lose();
+		if(GUI.Button(Rect(0, 0, Screen.width, Screen.height/2), endResult ))
+			exit();
+
+		// high scores
+		for(var i = 0; i < 10; i++)
+			GUI.Box(Rect(0, Screen.height/2 + Screen.height/20*i, Screen.width, Screen.height/20), i+1 + ": " + Mathf.Round(scores[i]*1000)/1000);
+
+	// game beginning
 	} else if(countDown > 0) {
-		GUI.skin = whiteSkin;
 		GUI.backgroundColor.a = 0;
-		GUI.Box(Rect(Screen.width/2-37, Screen.height/2-37, 75, 75), "" + Mathf.Round(countDown+.5));
+		GUI.Button(Rect(0, 0, Screen.width, Screen.height), "" + Mathf.Round(countDown+.5));
+
 	} else if(paused) {
-		GUI.skin = whiteSkin;
+		GUI.backgroundColor.a = 0;
 		if(GUI.Button(Rect(0,0,Screen.width,Screen.height),"Resume"))
 			paused = !paused;
+
 	} else {
-		var rows:int;
-		var cols:int;
-		for(rows = 0; rows < 4; rows++) {
-			if(rows >= board.length) {
-				GUI.skin = skin;
-				GUI.Box(Rect(0,0,Screen.width, Screen.height/4*(4-rows)),"");
-				break;
-			}
-			for(cols = 0; cols < 4; cols++) {
-				if((board[rows] as Array)[cols] == 1) {
-					GUI.skin = skin;
-					//GUI.color = Color.black;
-					//GUI.backgroundColor = Color.black;
-					if(GUI.Button(Rect(Screen.width/4*cols, Screen.height/4*(3-rows),Screen.width/(4), Screen.height/4),rock))
-						if(rows == 0) {
-							generateNext();
-						}
+		var row:int;
+		var col:int;
+		for(row = 0; row < 4; row++) {
+			for(col = 0; col < 4; col++) {
+				if(board[row*4 + col] > 0) {
+					if(GUI.Button(Rect(Screen.width/4*col, Screen.height/4*(3-row),Screen.width/(4), Screen.height/4),
+						rock_textures[board[row*4 + col]]))
+						check(col);
+				} else if(board[row*4] == -1) {
+					GUI.backgroundColor.a = 0;
+					if(GUI.Button(Rect(Screen.width/4*col, Screen.height/4*(3-row),Screen.width/(4), Screen.height/4), ""))
+						check(col);
 				} else {
-					GUI.skin = whiteSkin;
-					GUI.color = Color.white;
-					GUI.backgroundColor = Color.white;
-					if(GUI.Button(Rect(Screen.width/4*cols, Screen.height/4*(3-rows),Screen.width/(4), Screen.height/4),""))
-						if(rows == 0)
-							lost = true;
+					if(GUI.Button(Rect(Screen.width/4*col, Screen.height/4*(3-row),Screen.width/(4), Screen.height/4),""))
+						check(col);
 				}
 			}
 		}
@@ -95,31 +109,40 @@ function OnGUI () {
 }
 
 function generateNext() {
-	board.shift();
+	var index:int;
+	for(index = 0; index < board.Length; index++) {
+		if(index + 4 < board.Length)
+			board[index] = board[index + 4];
+		else if(board[index] == -1)
+			break;
+		else
+			board[index] = -1;
+	}
 	score++;
 	if(maxScore > 0 && score + 3 >= maxScore)
 		return;
 	var col:int;
-	board[3] = new Array(4);
-	var index = Random.Range(0,4);
+	index = Random.Range(0,4);
 	for(col = 0; col < 4; col++) {
 		if(index == col) {
-			(board[3] as Array)[col] = 1;
+			board[12 + col] = Random.Range(1, rock_textures.Length);
 		} else {
-			(board[3] as Array)[col] = 0;
+			board[12 + col] = 0;
 		}
 	}
 }
 
 function check(i:int) {
-	if((board[0] as Array)[i] == 1) {
+	if(board[i] > 0) {
 		generateNext();
 	} else {
 		lost = true;
 	}
 }
 
-function lose() {
+function exit() {
+	// writeout highscores
+	File.WriteAllText("scores.txt", scores.ToString());
 	Application.LoadLevel(0);
 }
 
@@ -154,6 +177,31 @@ function Update () {
 	} else {
 		timer -= Time.deltaTime;
 	}
-	if(maxTime > 0 && time > maxTime || maxScore > 0 && score >= maxScore)
+	if(!lost && (maxTime > 0 && time >= maxTime || maxScore > 0 && score >= maxScore)) {
 		lost = true;
+		var appended:double = time;
+		var temp:double;
+		for(var i = 0; i < 10; i++) {
+			if(appended < scores[i]) {
+				temp = appended;
+				appended = scores[i];
+				scores[i] = temp;
+			}
+//			if(scores[i] == 0) {
+//				if(appended)
+//				scores[i] = appended;
+//				break;
+//			}
+//			if(appended >= 0) {
+//				var temp = appended;
+//				appended = scores[i];
+//				scores[i] = temp;
+//			} else {
+//				if(time < scores[i]) {
+//					appended = scores[i];
+//					scores[i] = time;
+//				}
+//			}
+		}
+	}
 }
